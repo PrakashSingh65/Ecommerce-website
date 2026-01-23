12744098b62119d2707f0e48ca73d7f68854866d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-24 02:18:58 +0530

    Add react-icons dependency and update Navbar component styling

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -14,23 +14,29 @@ const navbar = () => {
         <Header className="bg-white">
             <nav className='max-w-[1400px] mx-auto px-10 h-[15vh] flex justify-between items-center'>
                 <a href="#" className='text-3xl font-bold'>
-                    Your<span className='text-green-400'>C</span>ompanay
+                    Your<span className='text-green-400 uppercase'>C</span>ompanay
                 </a>
 
-                <ul className='flex'>
+                <ul className='flex items-center gap-x-15'>
                     <li>
-                        <a href="#">Home</a>        
+                        <a href="#" className='font-semibold traking-wider text-zinc-700 hover:text-green-400'>Home</a>        
                     </li>
                     <li>
-                        <a href="#">About Us</a>        
+                        <a href="#" className='font-semibold traking-wider text-zinc-700 hover:text-red-700'>About Us</a>        
                     </li>
                     <li>
-                        <a href="#">Process</a>        
+                        <a href="#" className='font-semibold traking-wider text-zinc-700 hover:text-yellow-700'>Process</a>        
                     </li>
                     <li>
-                        <a href="#">Contect Us</a>        
+                        <a href="#" className='font-semibold traking-wider text-zinc-700 hover:text-green-400'>Contect Us</a>        
                     </li>
                 </ul>
+
+                <div>
+                    <a href="#">
+                        
+                    </a>
+                </div>
             </nav>
         </Header>
     </div>
