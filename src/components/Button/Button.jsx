a1de9c2fe606ab4511394f5383d34764d8ed229e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-26 17:13:35 +0530

    Refactor Button component to accept props and update Hero section to pass content

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Button = () => {
+const Button = (props) => {
   return (
-    <button className='bg-gradient-to-b from-orange-400 to-orange-500
-    text-white px-8 py-3'>
-        Book Now
+    <button className='bg-gradient-to-b from-orange-400 to-green-500
+    text-white px-8 py-3 rounded-lg text-lg hover:scale-105 hover:from-orange-600 
+    transition-all duration-300 cursor-pointer'>
+        {props.content}
     </button>
   )
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,7 +5,7 @@ import Button from '../Button/Button'
 const Hero = () => {
   return (
     <section>
-        <div className='max-w-\[1400px\] mx-auto px-10'>
+        <div className='max-w-\[1400px\] mx-auto px-10 flex item-center'>
             <div>
               <span className='bg-green-100 text-red-500 text-lg px-5 py-2 rounded-full'>Best Mobile Phone</span>
               <h1 className='text-5xl font-bold'>
@@ -14,7 +14,7 @@ const Hero = () => {
               <p className='text-zinc-500 text-lg'>
                 Build your online store effortlessly with Blocksy’s mobile-friendly
               </p>
-              <Button/>
+              <Button content="Book Now"/>
             </div>
             <div>
                 <img src={herophone} alt="Hero" />
