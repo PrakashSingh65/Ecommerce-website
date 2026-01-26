1bc43d86eeafa5ce0057916a00c84396a777291a
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-26 17:03:15 +0530

    Add Button component and enhance Hero section with improved styling and content

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='bg-gradient-to-b from-orange-400 to-orange-500
+    text-white px-8 py-3'>
+        Book Now
+    </button>
+  )
+}
+
+export default Button
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import herophone from '../../assets/unnamed.jpg'
+import Button from '../Button/Button'
 
 const Hero = () => {
   return (
     <section>
         <div className='max-w-\[1400px\] mx-auto px-10'>
             <div>
-              <span className='bg-green-100 text-red-500 text-lg'>Best Mobile Phone</span>
+              <span className='bg-green-100 text-red-500 text-lg px-5 py-2 rounded-full'>Best Mobile Phone</span>
+              <h1 className='text-5xl font-bold'>
+                Register with a valid GSTIN and an active <span className='text-red-700'>Bank</span> <span className='text-red-700'>Account</span> to become an Your<span className='text-green-400 uppercase'>C</span>ompany.in seller.
+              </h1>
+              <p className='text-zinc-500 text-lg'>
+                Build your online store effortlessly with Blocksy’s mobile-friendly
+              </p>
+              <Button/>
             </div>
             <div>
                 <img src={herophone} alt="Hero" />
