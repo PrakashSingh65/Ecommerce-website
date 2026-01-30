26f8645bfa1c15c52f4d6d5d2a3b53c32fd488ce
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-30 23:55:06 +0530

    feat: Add new `Heading` and `Category` components along with various image and SVG assets.

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
+import iphoneImage from '../../assets/iphone.jpeg'
 
 const Category = () => {
   return (
     <section>
         <div className='py-20'>
-            <Heading/>
+            <Heading highlight="Premium " heading="Phone"/>
         </div>
     </section>
   )
 }
 
 export default Category
+
+
+const category=[
+  {
+    id:1,
+    title: 'iPhone',
+    descritption:'The best iPhone models available now',
+    image:'iphoneImage',
+  }
+  {
+    id:2,
+    title: 'iPhone',
+    descritption:'The best iPhone models available now',
+    image:'iphoneImage',
+  }
+  {
+    id:3,
+    title: 'iPhone',
+    descritption:'The best iPhone models available now',
+    image:'iphoneImage',
+  }
+]
