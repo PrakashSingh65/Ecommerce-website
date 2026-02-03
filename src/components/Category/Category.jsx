2929b7ad3871faa6831212f1ecf69f76c528b16e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-02-03 17:43:09 +0530

    feat: Add button to category items and enhance styling for better user experience

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -13,41 +13,49 @@ const categoryimage=[
     title: 'Apple iPhone 17',
     descritption:'The best iPhone models available now',
     image:iphoneImage,
+    button:'Buy Now',
   },
   {
     id:2,
     title: 'Samsung Galaxy S24 5G',
     descritption:'The best Samsung models available now',
     image:samsungImage,
+    button:'Buy Now',
   },
   {
     id:3,
     title: 'MOTOROLA Edge 70',
     descritption:'The best Motorola models available now',
     image:motorolaImage,
+    button:'Buy Now',
   },
   {
     id:4,
     title: 'vivo',
     descritption:'The best vivo models available now',
     image:vivo,
+    button:'Buy Now',
   },
   {
     id:5,
     title: 'realme',
     descritption:'The best realme models available now',
     image:realme,
+    button:'Buy Now',
   },
   {
     id:6,
     title: 'pixel',
     descritption:'The best pixel models available now',
     image:pixel,
+    button:'Buy Now',
   }
 ]
 
 const Category = () => {
+
   return (
+
     <section>
         <div className='max-w-[1400px] max-auto px-10 py-20'>
             <Heading highlight="Premium " heading="Phone"/>
@@ -58,11 +66,14 @@ const Category = () => {
 
             <div className="mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
               {categoryimage.map(item => (
-                <div key={item.id} className="bg-orange-100 rounded-xl shadow-md overflow-hidden">
+                <div key={item.id} className="bg-orange-300 rounded-xl shadow-md overflow-hidden">
                   <img src={item.image} alt={item.title} className="w-full h-48 object-contain" />
-                  <div className="p-4">
+                  <div className="p-6">
                     <h3 className="text-zinc-800 text-3xl font-bold">{item.title}</h3>
                     <p className="text-zinc-600 mt-3 mb-9">{item.descritption}</p>
+                    <button className="bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green-600 transition-colors duration-300">
+                      {item.button ? item.button : 'Explore More'}
+                    </button>
                   </div>
                 </div>
               ))}
